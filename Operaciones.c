0db4a7907ce055bd077c55c730ab1a061c7c074b
Ricardo <rdkardo@gmail.com>
2015-11-03 09:07:06 -0600

    Create Operaciones.c

---
+++ Operaciones.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operaciones.h"
+
+void inicializar(struct nodo *cabecera)
+{
+	cabecera->sig = NULL;
+}
+
+void borrar_lista(struct nodo *cabecera)
+{
+	struct nodo *actual;
+	struct nodo *borrar;
+
+	/* Si es diferente de nulo borrar lista */
+	if (cabecera->sig != NULL) {
+		actual = cabecera->sig;
+		borrar = cabecera->sig;
+		/* Mientras el siguiente sea diferente de nulo */
+		while ( actual->sig != NULL) {
+			actual = actual->sig;
+			free(borrar);
+			borrar = actual;
+		}
+		free(borrar);
+		cabecera->sig = NULL;
+	}
+}
+
+void insertar_al_final(struct nodo *cabecera, int valor_nuevo)
+{
+	struct nodo *nuevo;
+	struct nodo *actual;
+	nuevo = (struct nodo*) malloc(sizeof(struct nodo) * 1);
+	nuevo->val = valor_nuevo;
